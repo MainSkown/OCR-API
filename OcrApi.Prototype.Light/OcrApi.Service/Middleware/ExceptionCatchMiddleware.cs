cf323388cd8cda72e052f938d370287ac9aaa393
MainSkown <83239861+MainSkown@users.noreply.github.com>
2021-07-07 15:58:54 +0200

    Prototype of OCR API
    Returns the JSON file, which is sended from OCR.

---
+++ OcrApi.Prototype.Light/OcrApi.Service/Middleware/ExceptionCatchMiddleware.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Serilog;
+using OcrApi.Data;
+
+namespace GithubApi.Service.Middleware
+{
+    public class ExceptionCatchMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+             
+        public ExceptionCatchMiddleware(RequestDelegate next, ILogger logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch(Exception ex)
+            {
+                ErrorResponse(ex, httpContext);
+            }   
+        }     
+        
+        async void ErrorResponse(Exception ex, HttpContext httpContext)
+        {
+            string message;
+            int statusCode;
+            _logger.Information($"Error occured at: {ex.Message}, type: {ex}");
+            Console.WriteLine($"Error occured at: {ex.Message}, type: {ex}");
+
+            switch (ex)
+            {
+                case HttpRequestException:                    
+                    message = "Couldn't find user";
+                    //It's possible to change message by adding a function that will change it acording to statusCode
+                    statusCode = (int)((HttpRequestException)ex).StatusCode;                    
+                    break;
+                
+                case OCRNotSucceededException:
+                    message = "OCR couldn't succsed operation";
+                    statusCode = StatusCodes.Status500InternalServerError;
+                    break;
+                
+
+                default:
+                    message = "Internal Server Error";
+                    statusCode = StatusCodes.Status500InternalServerError;
+                    break;
+            }
+
+            httpContext.Response.StatusCode = statusCode;
+            await httpContext.Response.WriteAsync(message);
+        }              
+     
+    }
+}
---
+++ OcrApi.Prototype.Light/OcrApi.Web/SetupOcr.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OcrApi.Web
+{
+    public static class SetupOcr
+    {
+        public static void SetupAzureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            IComputerVisionClient client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(configuration.GetSection("Key").Value))
+            { Endpoint = configuration.GetSection("Endpoint").Value };
+            services.AddSingleton(client);
+        }
+    }
+}
