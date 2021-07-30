a53f22cf39cd3ddec6ff8b2b87ea0537ec6ccd07
MainSkown <83239861+MainSkown@users.noreply.github.com>
2021-07-30 22:09:26 +0200

    Update 1.0
    Fixed file OCR.

---
+++ OcrApi.Light/OcrApi.Data/FileClass.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text.Json.Serialization;
+
+namespace OcrApi.Data
+{
+    public class FileClass
+    {
+        [JsonPropertyName("Image")]
+        public IFormFile Image { get; set; }
+    }
+}
---
+++ OcrApi.Light/OcrApi.Data/UrlClass.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OcrApi.Data
+{
+    public class UrlClass
+    {
+        [JsonPropertyName("url")]
+        public string url { get; set; }
+    }
+}
