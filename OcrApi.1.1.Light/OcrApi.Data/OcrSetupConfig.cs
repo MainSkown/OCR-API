ca0e22639a459e538728eb1d1116e7d37d2b7871
MainSkown <83239861+MainSkown@users.noreply.github.com>
2021-08-24 23:27:30 +0200

    Update 1.1

---
+++ OcrApi.1.1.Light/OcrApi.Data/OcrSetupConfig.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace OcrApi.Data
+{
+    public class OcrSetupConfig
+    {
+        public string Key { get; set; }
+
+        public string Endpoint { get; set; }
+    }
+}
---
+++ OcrApi.1.1.Light/OcrApi.Repo/IOcrService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using OcrApi.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OcrApi.Repo
+{
+    public interface IOcrService
+    {
+        Task<OcrTextClass> ReadApiUrl(string Url);
+        Task<OcrTextClass> ReadApiFile(IFormFile Image);
+    }
+}
